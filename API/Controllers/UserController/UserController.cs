b7798a295805dc32ce8500cbb1336650b9d90016
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2023-12-11 08:14:17 +0100

    added User and, UserDto. Just started on controller

---
+++ API/Controllers/UserController/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.UserController
+{
+    public class UserController : Controller
+    {
+        public static User user = new User();
+    }
+}
--- Domain/Models/User/User.cs
+++ Domain/Models/User/User.cs
@@ -8,5 +8,9 @@ namespace Domain.Models.User
 {
     public class User
     {
+        public Guid UserId { get; set; }
+        public required string UserName { get; set; } = string.Empty;
+        public required string UserPasswordHash { get; set; } = string.Empty;
+
     }
 }
